965fac92905a2b3cdf517b88f94153f6857b56e3
aashiq-q <iamashiqofficial@gmail.com>
2024-10-16 21:15:07 +0530

    Changed DP

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Style from './Home.module.scss';
-import me from '../../img/aashiq.png';
+import me from '../../img/aashiqq.png';
 import classNames from 'classnames';
 import EmojiBullet from "./EmojiBullet";
 import SocialIcon from "./SocialIcon";
